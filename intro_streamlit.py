6610fa6bab8dcdd988c30c30d1eadc219b9d93d3
eritsi <eritsi@gmail.com>
2022-08-06 06:12:43 +0000

    update first example

--- intro_streamlit.py
+++ intro_streamlit.py
@@ -17,10 +17,14 @@ def load_data():
 df = load_data()
 # ------------------------------------------------------------------------------
 
-# Sidebar - Sector selection
+# Sector selection
 sorted_year_unique = sorted(df['Year'].unique())
 selected_year = st.selectbox(
     'Year', sorted_year_unique)
+
+bee_killers = ["Disease", "Other", "Pesticides", "Pests_excl_Varroa", "Unknown", "Varroa_mites"]
+selected_killers = st.selectbox(
+    'Year', bee_killers)
 
 # Filtering data
 df_selected_year = df[df['Year']==selected_year]
@@ -29,6 +33,7 @@ st.write('The year chosen by user was:  ' +
          str(selected_year))
 
 dff = df_selected_year[df_selected_year['Affected by']=='Varroa_mites']
+dff_killers = df[df['Affected by']==selected_killers]
 
 fig = px.choropleth(
         data_frame=dff,
@@ -43,3 +48,25 @@ fig = px.choropleth(
     )
 
 st.plotly_chart(fig)
+
+fig2 = px.bar(
+    data_frame=dff,
+    x='State',
+    y='Pct of Colonies Impacted',
+    hover_data=['State', 'Pct of Colonies Impacted'],
+    labels={'Pct of Colonies Impacted': '% of Bee Colonies'},
+    template='plotly_dark'
+)
+fig2.update_layout(width=1000,height=500)
+st.plotly_chart(fig2)
+
+dff_killers = dff_killers[(dff_killers["State"] == "Idaho") | (dff_killers["State"] == "New York") | (dff_killers["State"] == "New Mexico")]
+
+fig3 = px.line(
+    data_frame=dff_killers,
+    x='Year',
+    y='Pct of Colonies Impacted',
+    color='State',
+    template='plotly_dark'
+)
+st.plotly_chart(fig3)
